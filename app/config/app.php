a3da2b009b92658d882aed58278ffa449c3b2da9
Sidharath <bladefury3@gmail.com>
2015-01-14 18:09:56 +0530

    Added Link

--- app/config/app.php
+++ app/config/app.php
@@ -23,7 +23,7 @@ return array(
 	| your application so that it is used when running Artisan tasks.
 	|
 	*/
-	'url' => 'http://localhost',
+	'url' => 'http://dailyart.in',
 	/*
 	|--------------------------------------------------------------------------
 	| Application Timezone
