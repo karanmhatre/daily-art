ea3ec1aa52181c392d4915e718a8a70ecb66623c
Sidharath <bladefury3@gmail.com>
2015-01-16 17:20:46 +0530

    Fixed'

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -24,7 +24,9 @@
 					<ul>
 						@foreach ($theme->art as $art)
 							<li class="item">
-								<a rel="gallery-{{$index}}" class="single_image swipebox" href="{{ URL::asset($art->image) }}" title="<a href={{ URL::route('art.show', $art->id) }}>{{ $theme->theme }}</a> by <a href={{ URL::route('user.profile', [$art->user->id, Str::slug($art->user->name)]) }}><img src={{ URL::asset($art->user->avatar) }} class='profile-picture light-box-picture'></a>">
+								<a rel="gallery-{{$index}}" class="single_image swipebox" href="{{ URL::asset($art->image) }}" title="<a href={{ URL::route('art.show', $art->id) }}>{{ $theme->theme }}</a> by <a href={{ URL::route('user.profile', [$art->user->id, Str::slug($art->user->name)]) }}><img src={{ URL::asset($art->user->avatar) }} class='profile-picture light-box-picture'></a><a href=http://www.facebook.com/sharer.php?u={{URL::route('art.show', [$art->id])}} target='_blank'><img src='http://www.simplesharebuttons.com/images/somacro/facebook.png' alt='Facebook' /></a>
+    			<a href=http://twitter.com/share?url={{URL::route('art.show', [$art->id])}}&text=Daily Art by {{ $art->user->name }}&hashtags=dailyart, genii target='_blank'><img src='http://www.simplesharebuttons.com/images/somacro/twitter.png' alt='Twitter' /></a>
+">
 									{{ HTML::image($art->image, $art->caption, ['title' => ''] ) }}
 								</a>
 		           </li>
