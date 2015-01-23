c8926799b6ef2163deb57f842d851c2ddc0dbeeb
astrozip <karan@onesquarepixel.com>
2015-01-23 23:02:26 +0530

    added GA

--- app/views/home/register.blade.php
+++ app/views/home/register.blade.php
@@ -1,121 +1,72 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Daily Art | Login</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
+@extends('layouts.master')
 
-    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-    {{ HTML::style('css/normalize.css') }}
-    {{ HTML::style('css/foundation.min.css') }}
-    {{ HTML::style('css/font-awesome.min.css') }}
-    {{ HTML::style('css/main.css') }}
-  </head>
-  <body>
-    <!--[if lt IE 7]>
-        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-    <![endif]-->
+@section('content')
+  <div class="row">
+    <div class="small-6 large-centered columns" id="login-container">
+      <h2>Daily<i class="fa fa-pencil"></i>Art Register</h2>
 
-    <!-- Add your site or application content here -->
-
-    <div class="row">
-      <div class="small-6 large-centered columns" id="login-container">
-        <h2>Daily<i class="fa fa-pencil"></i>Art Register</h2>
-
-        @if(Session::has('message'))
-          <div class="alert-box danger">
-            {{ Session::get('message') }}
-          </div>
-        @endif
+      @if(Session::has('message'))
+        <div class="alert-box danger">
+          {{ Session::get('message') }}
+        </div>
+      @endif
 
 
-        {{ Form::open(array('route' => 'user.store','files' => 'true')) }}
-          <div class="small-10 large-centered columns">
-            <div class="row">
-              <div class="small-3 columns">
-                <label for="right-label" class="right inline">Name</label>
-              </div>
-              <div class="small-9 columns">
-                <input type="text" id="right-label" placeholder="Name" name="name" value="{{ $user->name }}" required>
-              </div>
+      {{ Form::open(array('route' => 'user.store','files' => 'true')) }}
+        <div class="small-10 large-centered columns">
+          <div class="row">
+            <div class="small-3 columns">
+              <label for="right-label" class="right inline">Name</label>
+            </div>
+            <div class="small-9 columns">
+              <input type="text" id="right-label" placeholder="Name" name="name" value="{{ $user->name }}" required>
             </div>
           </div>
-          <input name="register_code" type="hidden" value="{{ $user->register_code }}">
-          <div class="small-10 large-centered columns">
-            <div class="row">
-              <div class="small-3 columns">
-                <label for="right-label" class="right inline">Email</label>
-              </div>
-              <div class="small-9 columns">
-                <input type="text" id="right-label" placeholder="Email" name="email" disabled value="{{ $user->email }}" required>
-              </div>
+        </div>
+        <input name="register_code" type="hidden" value="{{ $user->register_code }}">
+        <div class="small-10 large-centered columns">
+          <div class="row">
+            <div class="small-3 columns">
+              <label for="right-label" class="right inline">Email</label>
+            </div>
+            <div class="small-9 columns">
+              <input type="text" id="right-label" placeholder="Email" name="email" disabled value="{{ $user->email }}" required>
             </div>
           </div>
-          <div class="small-10 large-centered columns">
-            <div class="row">
-              <div class="small-3 columns">
-                <label for="right-label" class="right inline">Avatar</label>
-              </div>
-              <div class="small-9 columns">
-                <input type="file" name="avatar" required>
-              </div>
+        </div>
+        <div class="small-10 large-centered columns">
+          <div class="row">
+            <div class="small-3 columns">
+              <label for="right-label" class="right inline">Avatar</label>
+            </div>
+            <div class="small-9 columns">
+              <input type="file" name="avatar" required>
             </div>
           </div>
-          <div class="small-10 large-centered columns">
-            <div class="row">
-              <div class="small-3 columns">
-                <label for="password" class="right inline">Password</label>
-              </div>
-              <div class="small-9 columns">
-                <input type="password" id="password" name="password" required>
-              </div>
+        </div>
+        <div class="small-10 large-centered columns">
+          <div class="row">
+            <div class="small-3 columns">
+              <label for="password" class="right inline">Password</label>
+            </div>
+            <div class="small-9 columns">
+              <input type="password" id="password" name="password" required>
             </div>
           </div>
-          <div class="small-10 large-centered columns">
-            <div class="row">
-              <div class="small-3 columns">
-              </div>
-              <div class="small-9 columns">
-                <input type="submit" class="button" value="Register"><br>
-                <p>Todays topic “<b>{{ $theme }}</b>”.</p>
-              </div>
+        </div>
+        <div class="small-10 large-centered columns">
+          <div class="row">
+            <div class="small-3 columns">
+            </div>
+            <div class="small-9 columns">
+              <input type="submit" class="button" value="Register"><br>
+              <p>Todays topic “<b>{{ $theme }}</b>”.</p>
             </div>
           </div>
-        {{ Form::close() }}
+        </div>
+      {{ Form::close() }}
 
-      </div>
     </div>
+  </div>
 
-
-    <!-- Scripts -->
-
-    {{ HTML::script('js/vendor/modernizr-2.6.2.min.js') }}
-    {{ HTML::script('js/vendor/jquery-1.9.0.min.js') }}
-    {{ HTML::script('js/vendor/foundation.min.js') }}
-    {{ HTML::script('js/vendor/polyfiller.js') }}
-    <script>
-        webshims.setOptions('forms', {
-        lazyCustomMessages: true
-    });
-
-    //start polyfilling
-    webshims.polyfill('forms');
-    webshim.polyfill('es5 mediaelement forms');
-    </script>
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script>
-    $(document).foundation();
-    //   var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-    //   (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    //   g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    //   s.parentNode.insertBefore(g,s)}(document,'script'));
-    </script>
-
-    @yield('scripts')
-  </body>
-</html>
+@stop
--- app/views/suggestions/new.blade.php
+++ app/views/suggestions/new.blade.php
@@ -1,43 +1,23 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('layouts.master')
 
-    <title>Daily Art | Make art, daily.</title>
-    {{ HTML::style('css/admin/pure-min.css') }}
-    {{ HTML::style('css/admin/main.css') }}
-    {{ HTML::style('css/admin/side-menu.css') }}
-    {{ HTML::style('css/admin/font-awesome.min.css')}}
-    {{ HTML::style('css/main.css') }}
-  </head>
-  <body class="login" style="background-image: url('../../img/{{$number}}.jpg'); background-size: cover;">
-    <div class="overlay"></div>
-    <div class="center suggestion">
-          <a href="{{ URL::to('/') }}"><h1 class="main_heading">Daily<i class="fa fa-pencil"></i>Art</h1></a>
-      <form class="pure-form pure-form-stacked suggestion-form" action="{{ URL::route('suggestions.store') }}" method="POST">
-        <fieldset>
-            <legend>Suggestions</legend>
-            <div class="inner-addon left-addon">
-              <i class="fa fa-pencil"></i>
-              <input id="topic" name="topic" type="text" placeholder="Topic" required>
-            </div>
-            <button type="submit" class="pure-button pure-button-primary">Submit</button>
-            @if(Session::has('notice'))
-              <div style="margin: 20px auto; color:red"><i class="fa fa-warning"></i> {{ Session::get('notice') }}</div>  
-            @endif
-        </fieldset>
-      </form>
-    </div>
-    {{ HTML::script('js/vendor/jquery.js') }}
-    {{ HTML::script('js/vendor/polyfiller.js') }}
-    <script>
-        webshims.setOptions('forms', {
-        lazyCustomMessages: true
-    });
+@section('content')
 
-    //start polyfilling
-    webshims.polyfill('forms');
-    </script>
-  </body> 
-</html>
+  <div class="overlay"></div>
+  <div class="center suggestion">
+        <a href="{{ URL::to('/') }}"><h1 class="main_heading">Daily<i class="fa fa-pencil"></i>Art</h1></a>
+    <form class="pure-form pure-form-stacked suggestion-form" action="{{ URL::route('suggestions.store') }}" method="POST">
+      <fieldset>
+          <legend>Suggestions</legend>
+          <div class="inner-addon left-addon">
+            <i class="fa fa-pencil"></i>
+            <input id="topic" name="topic" type="text" placeholder="Topic" required>
+          </div>
+          <button type="submit" class="pure-button pure-button-primary">Submit</button>
+          @if(Session::has('notice'))
+            <div style="margin: 20px auto; color:red"><i class="fa fa-warning"></i> {{ Session::get('notice') }}</div>
+          @endif
+      </fieldset>
+    </form>
+  </div>
+
+@stop
