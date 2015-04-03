eb997848afdde2f49c21ec741c329a401f698c3a
astrozip <karan@onesquarepixel.com>
2015-04-03 18:13:14 +0530

    fixes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -42,6 +42,8 @@ class UserController extends \BaseController {
 	{
 		$art = Art::find($id);
 
+		$user->current_streak = $user->current_streak - 1;
+
 		if($art->user_id == Auth::user()->id)
 			Art::find($id)->delete();
 
